e8bbfa1279ace569c2fc94739d8032ed94159ddc
LoganBanning <loganboganb@gmail.com>
2025-08-19 21:34:53 -0600

    added more to about area

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,13 @@
 import "./About.css"
 import { useState } from "react"
 import TextLoop from "../utils/TextLoop"
+import LoganHeadshot from "../../assets/Images/headshot-logan.jpg"
 
 const About = () => {
 
     return (
         <div className="about_container">
-            <h1>Hi, I'm Logan<br/>
+            <h1 className="intro_text">Hi, I'm Logan.<br/>
                 <TextLoop
                     items={[
                         "React.js",
@@ -18,6 +19,10 @@ const About = () => {
                         "C#",]} />
             </h1>
             <h2>Full Stack Web Developer</h2>
+            <div className="about_me_container">
+                <img className="headshot" src={LoganHeadshot} alt="headshot of me" />
+                <h2>About Me</h2>
+            </div>
         </div>
     )
 }
