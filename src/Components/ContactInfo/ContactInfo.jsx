c2e1b5690e621738897fdfd4c13109539fb3ed21
LoganBanning <loganboganb@gmail.com>
2025-08-26 21:21:21 -0600

    set up contact section

--- src/Components/ContactInfo/ContactInfo.jsx
+++ src/Components/ContactInfo/ContactInfo.jsx
@@ -1,9 +1,18 @@
 import './ContactInfo.css'
+import LoganHikingPicture from '../../assets/Images/Logan-picture-bell-canyon.jpg'
 
 const ContactInfo = () => {
 
     return (
         <div className="contact_info_container">
+            <div className="contact_text_container">
+                <h2 className="contact_text_one">Hire Me.</h2>
+                <p className="description">I look forward to working with you! <br />
+                    <a className="contact_link" href="tel:+8018858699">Phone: 801.885.8699</a> <br />
+                    <a className="contact_link" href="mailto:loganbanning2@gmail.com">Email: loganbanning2@gmail.com</a>
+                </p>
+            </div>
+            <img className="hiking_picture" src={LoganHikingPicture} alt="hiking picture of me" />
         </div>
     )
 }
--- src/Components/TopNav/TopNav.jsx
+++ src/Components/TopNav/TopNav.jsx
@@ -10,17 +10,17 @@ const SocialShare = [
 const TopNav = () => {
     return (
         <header className="top_nav_container">
-            <nav className="nav_container">
+            {/* <nav className="nav_container">
                 <label className="nav_label">Home</label>
                 <label className="nav_label">About</label>
                 <label className="nav_label">Portfolio</label>
                 <label className="nav_label">Contact</label>
-            </nav>
+            </nav> */}
              <div className="header_right">
                 <div className="social_share_inner">
                     <ul className="social_share">
                         {SocialShare.map((val , i) => (
-                            <li key={i}><a href={`${val.link}`} target='_blank'>{val.Social}</a></li>
+                            <li key={i}><a className="social_links" href={`${val.link}`} target='_blank'>{val.Social}</a></li>
                         ))}
                     </ul>
                 </div>
